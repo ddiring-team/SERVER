d9f184ba7c0b0ef9c1f41b6b6911922afd1b429b
unrookie21 <piresj0318@gmail.com>
2026-03-01 16:26:39 +0900

    Project Initialization

---
+++ src/main/java/com/ddiring/ddiring_server/DdiringServerApplication.java
@@ -0,0 +1,13 @@
+package com.ddiring.ddiring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdiringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdiringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddiring/ddiring_server/DdiringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddiring.ddiring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdiringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
